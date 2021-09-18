a5f88150df65370efc2bf355e6ce41b06a82afad
Anurag Shukla <www.anuragshukla@gmail.com>
2021-09-18 10:50:03 +0530

    commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function submitForm(){
+    var xhr = new XMLHttpRequest() ;
+    xhr.open("POST", "server.php");
+    xhr.onreadystatechange = function() {
+         if(xhr.readyState==4) {
+         alert(xhr.status);
+     }
+}
+        var formData = new FormData(document.getElementById("form")); 
+        xhr.send(formData);
+}
